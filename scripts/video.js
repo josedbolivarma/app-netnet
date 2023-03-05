58311a0cc938fced297c657e140a1c009b2ec982
Jose David Bolivar Mayora <77859779+josedbolivarma@users.noreply.github.com>
2023-03-05 18:09:31 -0500

    Update video.js

--- scripts/video.js
+++ scripts/video.js
@@ -18,7 +18,7 @@ const $freaturedDetailsContainer = d.getElementById("freatured-details");
 
 async function getMovies (){
     try {
-        const resp = await fetch('https://app-netnet.herokuapp.com/movies');
+        const resp = await fetch('https://netnetbackendexpress.onrender.com/movies');
         const data = await resp.json()
         return data
     }catch (error){
