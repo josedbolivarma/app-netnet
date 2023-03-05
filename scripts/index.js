1caf4ea2cd1c98520c9e13f9361b780f69b8ccd8
Jose David Bolivar Mayora <77859779+josedbolivarma@users.noreply.github.com>
2023-03-05 18:07:47 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ $SeriesEEUUContainer = d.getElementById("seriesEEUU");
 
 async function getMovies(){
     try {
-        const resp = await fetch("https://app-netnet.herokuapp.com/movies");
+        const resp = await fetch("https://netnetbackendexpress.onrender.com/movies");
         const data = await resp.json();
         return data;
     }catch(err){
